6059c5383eeb705e6da5d172024c9b2132affbb3
naveenchunchu <chunchunaveen2001@gmail.com>
2022-05-16 15:55:31 +0530

    Done..

---
+++ boot-s3-integration/src/test/java/com/cts/boot/BootS3IntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootS3IntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
